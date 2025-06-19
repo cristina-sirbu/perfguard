93d731ba09cbf4caf8b0d30a28ef25ee3ed758dc
Cristina Junca (Sirbu) <sirbucristina.is@gmail.com>
2025-06-19 10:48:25 +0200

    Initial version of the PerfGuard app

---
+++ src/main/java/com/perfguard/perfguard/PerfguardApplication.java
@@ -0,0 +1,13 @@
+package com.perfguard.perfguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfguard/perfguard/PerfguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfguard.perfguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
